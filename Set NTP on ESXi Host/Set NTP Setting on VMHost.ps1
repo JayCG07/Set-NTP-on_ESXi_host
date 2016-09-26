3564025b489d94b5f3a30e52a8647bb00593dca5
JayCG07 <golden.jay@gmail.com>
2016-09-26 18:58:11 -0400

    Add new Sciprt

---
+++ Set NTP on ESXi Host/Set NTP Setting on VMHost.ps1
@@ -0,0 +1,11 @@
+﻿
+$esx = "host.domain.com"
+
+#Configure NTP server
+Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esx 
+Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esx 
+#Allow NTP queries outbound through the firewall
+Get-VMHostFirewallException -VMHost $esx | where {$_.Name -eq "NTP client"} | Set-VMHostFirewallException -Enabled:$true
+#Start NTP client service and set to automatic
+Get-VmHostService -VMHost $esx | Where-Object {$_.key -eq "ntpd"} | Start-VMHostService
+Get-VmHostService -VMHost $esx | Where-Object {$_.key -eq "ntpd"} | Set-VMHostService -policy "automatic"

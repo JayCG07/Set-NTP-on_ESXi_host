1c11e959f774fbfdfda45b877f21c7a0cc8096f4
JayCG07 <golden.jay@gmail.com>
2016-09-26 22:13:32 -0400

    Changed code for fun.

--- Set NTP on ESXi Host/Set NTP Setting on VMHost.ps1
+++ Set NTP on ESXi Host/Set NTP Setting on VMHost.ps1
@@ -1,12 +1,14 @@
 ﻿
 #----Add host name
-$esx = "host1.domain.com"
+$esxihost = "host1.domain.com"
 
-#Configure NTP server
-Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esx 
-Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esx 
-#Allow NTP queries outbound through the firewall
-Get-VMHostFirewallException -VMHost $esx | where {$_.Name -eq "NTP client"} | Set-VMHostFirewallException -Enabled:$true
-#Start NTP client service and set to automatic
-Get-VmHostService -VMHost $esx | Where-Object {$_.key -eq "ntpd"} | Start-VMHostService
-Get-VmHostService -VMHost $esx | Where-Object {$_.key -eq "ntpd"} | Set-VMHostService -policy "automatic"
+#----Configure NTP server
+Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esxihost 
+Add-VmHostNtpServer -NtpServer "dmz.domain.com" -VMHost $esxihost 
+
+#----Setup firewall 
+Get-VMHostFirewallException -VMHost $esxihost | where {$_.Name -eq "NTP client"} | Set-VMHostFirewallException -Enabled:$true
+
+#----Start NTP service and set to automatic
+Get-VmHostService -VMHost $esxihost | Where-Object {$_.key -eq "ntpd"} | Start-VMHostService
+Get-VmHostService -VMHost $esxihost | Where-Object {$_.key -eq "ntpd"} | Set-VMHostService -policy "automatic"
